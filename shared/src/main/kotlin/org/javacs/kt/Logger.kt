1d37498d009a414684fd8e22c340c51ff4092872
Aleksey Krichevskiy <boolivar@gmail.com>
2026-05-14 23:58:56 +0300

    feat: BackendWriter rwLock

--- shared/src/main/kotlin/org/javacs/kt/Logger.kt
+++ shared/src/main/kotlin/org/javacs/kt/Logger.kt
@@ -3,44 +3,56 @@ package org.javacs.kt
 import org.tinylog.core.LogEntry
 import org.tinylog.jul.JulTinylogBridge
 import org.tinylog.writers.AbstractFormatPatternWriter
+import java.util.concurrent.ConcurrentLinkedQueue
+import java.util.concurrent.locks.ReentrantReadWriteLock
 import kotlin.concurrent.Volatile
+import kotlin.concurrent.read
+import kotlin.concurrent.write
 
 typealias LOG = org.tinylog.kotlin.Logger
+
 typealias LogLevel = org.tinylog.Level
 
 class LogMessage(val level: LogLevel, val message: String)
 
 object BackendWriter: AbstractFormatPatternWriter(mapOf()) {
 
+    private val queue = ConcurrentLinkedQueue<LogMessage>()
+
+    private val rwLock = ReentrantReadWriteLock()
+
+    private var backend: (LogMessage) -> Unit = queue::add
+
     @Volatile
     var level = LogLevel.TRACE
 
-    var backend: ((LogMessage) -> Unit)? = null
-        set(value) {
-            field = value
-            while (preliminaryQueue.isNotEmpty()) {
-                write(preliminaryQueue.removeFirst())
-            }
+    fun connect(backend: (LogMessage) -> Unit) = rwLock.write {
+        while (queue.isNotEmpty()) {
+            backend.invoke(queue.poll())
         }
+        this.backend = backend
+    }
 
-    val preliminaryQueue = ArrayDeque<LogEntry>()
+    fun disconnect() = rwLock.write {
+        this.backend = queue::add
+    }
 
     override fun write(logEntry: LogEntry) {
         if (logEntry.level >= level) {
-            backend?.invoke(LogMessage(logEntry.level, render(logEntry))) ?: preliminaryQueue.add(logEntry)
+            rwLock.read {
+                backend.invoke(LogMessage(logEntry.level, render(logEntry)))
+            }
         }
     }
 
     override fun flush() {}
 
-    override fun close() {}
+    override fun close() = disconnect()
 }
 
 fun LOG.connectJULFrontend() = JulTinylogBridge.activate()
 
-fun LOG.connectOutputBackend(backend: (LogMessage) -> Unit) {
-    BackendWriter.backend = backend
-}
+fun LOG.connectOutputBackend(backend: (LogMessage) -> Unit) = BackendWriter.connect(backend)
 
 fun LOG.connectErrorBackend(backend: (LogMessage) -> Unit) {}
 
